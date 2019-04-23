e93d1874e618088c681f143a1036b1ce4a4a75c8
kenyangnov <2275169040@qq.com>
2019-04-23 10:30:47 +0800

    more pythonic

--- python/532. 数组中的K-diff数对.py
+++ python/532. 数组中的K-diff数对.py
@@ -1,4 +1,8 @@
 # collections.Counter和set的使用
+
+# Counter类的目的是跟踪值出现的次数。
+# 它是一个无序的容器类型，以字典的键值对形式存储，其中元素作为key，其计数作为value。
+
 class Solution(object):
     def findPairs(self, nums, k):
         """
@@ -21,3 +25,18 @@ class Solution(object):
                 if (i+k) in s:
                     cnt+=1
             return cnt
+			
+# more pythonic
+class Solution(object):
+	def findPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+		if k<0:
+			return 0
+		elif k==0:
+			return sum(v>1 for v in collection.Counter(nums).values())
+		else:
+			return len(set(nums) & set(i+k for i in nums))
