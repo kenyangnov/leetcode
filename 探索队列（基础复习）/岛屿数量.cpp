cf0a155ca5172afc6b14e2fbd3f6312dce23cee9
kynov <kynov@outlook.com>
2019-09-29 22:41:55 +0800

    pair和vector的使用

--- 探索队列（基础复习）/岛屿数量.cpp
+++ 探索队列（基础复习）/岛屿数量.cpp
@@ -3,6 +3,52 @@ private:
     int rows;
     int cols;
     int cnt;
+    int dir[4][2] = { 0,1,0,-1,1,0,-1,0 };
+    void BFS(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& visited){
+        queue<pair<int,int>> q;
+        q.push(make_pair(i,j));
+        visited[i][j]=1;
+        while(!q.empty()){
+            int cur_i = q.front().first;
+            int cur_j = q.front().second;
+            int temp_i,temp_j;
+            for(int i=0;i<4;i++){
+                temp_i = cur_i + dir[i][0];
+                temp_j = cur_j + dir[i][1];
+                if(temp_i>=0 && temp_i<rows && temp_j>=0 && temp_j<cols)    //注意判断边界
+                    if(grid[temp_i][temp_j]=='1' && visited[temp_i][temp_j]==0){
+                        q.push(make_pair(temp_i, temp_j));
+                        visited[temp_i][temp_j]=1;
+                }
+            }
+            q.pop();
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.size()==0)return 0;
+        rows = grid.size();
+        cols = grid[0].size();
+        cnt = 0;
+        vector<vector<bool>> visited(rows,vector<bool>(cols,0)); //初始化访问标记数组
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1' && visited[i][j]==0){
+                    cnt++;
+                    BFS(i, j, grid, visited);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+/*
+class Solution {
+private:
+    int rows;
+    int cols;
+    int cnt;
     struct Loc{
         int i;
         int j;
@@ -51,3 +97,4 @@ public:
         return cnt;
     }
 };
+*/
