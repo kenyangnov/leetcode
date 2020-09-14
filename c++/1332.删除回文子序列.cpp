be4ecaadeca14d15ac39fbef503b933fecdcb7cf
kyn0v <2275169040@qq.com>
2020-09-14 21:47:56 +0800

    skill: 子序列≠字串

---
+++ c++/1332.删除回文子序列.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(s.length()==0){
+            return 0;
+        }
+        int i=0, j=s.length()-1;
+        for(;i<j; i++,j--){
+            if(s[i]!=s[j]){
+                return 2;
+            }
+        }
+        return 1;
+    }
+};
