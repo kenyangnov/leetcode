f8da15661b5d9eb24b7e765d47c989c3104667a2
kyn0v <2275169040@qq.com>
2020-09-24 18:45:15 +0800

    fix: 确保"t->member"时t非空

--- c++/617.合并二叉树.cpp
+++ c++/617.合并二叉树.cpp
@@ -14,18 +14,35 @@ public:
     }
 };
 
-//WA
 class Solution2 {
-public:
+   public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
         if ((t1 == nullptr) && (t2 == nullptr)) {
             return nullptr;
         }
-        int a = (t1==NULL)?0:t1->val;
-        int b = (t2==NULL)?0:t2->val;
+        int a, b;
+        TreeNode *a_left, *a_right, *b_left, *b_right;
+        if (t1 == nullptr) {
+            a = 0;
+            a_left = nullptr;
+            a_right = nullptr;
+        } else {
+            a = t1->val;
+            a_left = t1->left;
+            a_right = t1->right;
+        }
+        if (t2 == nullptr) {
+            b = 0;
+            b_left = nullptr;
+            b_right = nullptr;
+        } else {
+            b = t2->val;
+            b_left = t2->left;
+            b_right = t2->right;
+        }
         auto t = new TreeNode(a + b);
-        t->left = mergeTrees(t1->left, t2->left);
-        t->right = mergeTrees(t1->right, t2->right);
+        t->left = mergeTrees(a_left, b_left);
+        t->right = mergeTrees(a_right, b_right);
         return t;
     }
 };
